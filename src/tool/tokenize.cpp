d32deb9b77d16eb9c34db09aba708872d92ed25c
xVKSx <xxvksxx@gmail.com>
2019-05-13 21:56:59 +0300

    Update tokenize.cpp

--- src/tool/tokenize.cpp
+++ src/tool/tokenize.cpp
@@ -6,6 +6,7 @@ std::vector<std::string> tokenize(const std::string &s, char delimiter) {
     std::vector<std::string> tokens;
     std::string token;
     std::istringstream tokenStream(s);
+    
     while (std::getline(tokenStream, token, delimiter)) {
         tokens.push_back(token);
     }
